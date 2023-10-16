7baf24d25aeb69f975cf96fb273aa7839b17c3be
gunerbeycmp <guneremr@gmail.com>
2023-10-16 16:14:53 +0300

    Initial Commit

---
+++ freelancerDesign/freelancerDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  freelancerDesignApp.swift
+//  freelancerDesign
+//
+//  Created by Emir Güner on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct freelancerDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
